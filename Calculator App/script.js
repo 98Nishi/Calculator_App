c8ccc5a8cc134afbedbb058d415ce1914b355454
Nisha Maurya <119358704+98Nishi@users.noreply.github.com>
2023-05-19 19:31:55 +0530

    Update script.js

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -1,27 +1,3 @@
-// Program for Calculator
 
-// getting HTML elements 
-let buttons = document.querySelector('.buttons');
-let btn =  document.querySelectorAll('button');
-let value = document.getElementById('value');
-
-
-for(let i=0; i < btn.length; i++)
-{
-    btn[i].addEventListener("click",function(){
-
-        if(this.innerHTML=="="){
-            value.innerHTML = eval(value.innerHTML);
-
-        } else {
-            if(this.innerHTML== "AC"){
-                value.innerHTML = "";
-            }
-            else{
-                value.innerHTML += this.innerHTML;
-            }
-        }
-    })
-}
 
   
